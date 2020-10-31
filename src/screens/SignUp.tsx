f021574795b12ae320707f0ac90d804ea3cad25e
keisukekashimura <kashimura@refcome.com>
2020-10-31 17:16:51 +0900

    fix sign up

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,39 +1,63 @@
 import React, { useState } from "react";
 import firebase from "../Firebase";
-import { Link, withRouter } from "react-router-dom";
+import { Link, withRouter, useHistory } from "react-router-dom";
+import { gql, useMutation } from "@apollo/client";
 
-export const SignUp = withRouter((props) => {
+const CREATE_USER = gql`
+  mutation CreateUser($name: String!, $email: String!) {
+    createUser(input: { name: $name, email: $email }) {
+      user {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
+
+export const SignUp = withRouter(() => {
+  const history = useHistory();
   const [loading, setLoading] = useState(false);
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [createUser, { data }] = useMutation(CREATE_USER);
 
-  const onSubmit = () => {
-    setLoading(true);
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(({ user }) => {
-        user?.sendEmailVerification();
-        setLoading(false);
-        props.history.push("/");
-      })
-      .catch((error) => {
-        setLoading(false);
-        alert(error);
-      });
+  const handleCreateUser = async () => {
+    try {
+      setLoading(true);
+      const { user } = await firebase.auth().createUserWithEmailAndPassword(email, password);
+      await createUser({ variables: { name: name, email: email } });
+      await user?.sendEmailVerification();
+      setLoading(false);
+      history.push("/");
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+      // TODO: Firebaseに既に登録されていたらアカウント登録処理を行うようにする
+      if (e.code === "auth/email-already-in-use") {
+        alert("既に利用されているメールアドレスです。");
+        return;
+      }
+
+      alert("登録に失敗しました。");
+    }
   };
 
   return (
     <div>
       <div>新規登録</div>
       <div>
-        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="text" onChange={(e) => setName(e.target.value)} placeholder="名前を入力" />
       </div>
       <div>
-        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="メールアドレスを入力" />
       </div>
       <div>
-        <button type="submit" onClick={onSubmit} disabled={loading}>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="パスワードを入力" />
+      </div>
+      <div>
+        <button type="submit" onClick={handleCreateUser} disabled={loading}>
           {loading ? "loading..." : "新規登録"}
         </button>
       </div>
