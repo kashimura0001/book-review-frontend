82584504e14d830553e7c6d13ec41cb074854fe5
keisukekashimura <kashimura@refcome.com>
2020-11-01 00:15:12 +0900

    fix sign in process

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -6,6 +6,7 @@ export const SignIn = withRouter((props) => {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [hasSignInError, setHasSignInError] = useState(false);
 
   const onSubmit = () => {
     setLoading(true);
@@ -22,14 +23,38 @@ export const SignIn = withRouter((props) => {
       });
   };
 
+  const handleSignUp = async () => {
+    setLoading(true);
+    setHasSignInError(false);
+
+    const user = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        return user;
+      })
+      .catch(() => {
+        return null;
+      });
+
+    if (!user) {
+      setHasSignInError(true);
+      setLoading(false);
+      return;
+    }
+
+    // TODO DBに確認する処理を実装する
+  };
+
   return (
     <div>
       <div>サインイン</div>
+      {hasSignInError && <div>サインインに失敗しました。</div>}
       <div>
-        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder="メールアドレスを入力" />
       </div>
       <div>
-        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="パスワードを入力" />
       </div>
       <div>
         <button type="submit" onClick={onSubmit} disabled={loading}>
