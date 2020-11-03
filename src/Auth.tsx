dbfe3bd4a115b7b0d4547a29246f5db62a1e6fa1
keisukekashimura <kashimura@refcome.com>
2020-11-04 00:31:29 +0900

    fix sign in process

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,14 +1,16 @@
 import React, { FC, useEffect, useState } from "react";
 import { Redirect } from "react-router-dom";
 import firebase from "./Firebase";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation } from "@apollo/client";
 
-const GET_CURRENT_USER = gql`
-  query GetCurrentUser {
-    currentUser {
-      id
-      email
-      name
+const SIGN_IN = gql`
+  mutation SignInUser {
+    signInUser(input: {}) {
+      user {
+        id
+        name
+        email
+      }
     }
   }
 `;
@@ -16,22 +18,30 @@ const GET_CURRENT_USER = gql`
 export const Auth: FC<{ children: any }> = ({ children }) => {
   const [isCheckCompleted, setIsCheckCompleted] = useState(false);
   const [isSignedIn, setIsSignedIn] = useState(false);
-  const { loading, data, refetch } = useQuery(GET_CURRENT_USER);
+  const [isRegisteredProfile, setIsRegisteredProfile] = useState(false);
+  const [signIn] = useMutation(SIGN_IN);
 
   useEffect(() => {
     return firebase.auth().onAuthStateChanged(async (user) => {
-      if (user) {
-        setIsSignedIn(true);
-        await refetch();
-      } else {
+      if (!user) {
         setIsSignedIn(false);
+        setIsCheckCompleted(true);
+        return;
+      }
+
+      try {
+        const result = await signIn();
+        setIsSignedIn(true);
+        setIsRegisteredProfile(!!result);
+      } catch (e) {
+        // TODO エラーである旨がわかるようにする
       }
       setIsCheckCompleted(true);
     });
   }, []);
 
-  if (!isCheckCompleted || loading) return <p>Loading...</p>;
+  if (!isCheckCompleted) return <p>Loading...</p>;
   if (!isSignedIn) return <Redirect to="signin" />;
-  if (isSignedIn && data?.currentUser) return children;
-  if (isSignedIn && !data?.currentUser) return <Redirect to="/profile/register" />;
+  if (isSignedIn && isRegisteredProfile) return children;
+  if (isSignedIn && !isRegisteredProfile) return <Redirect to="/profile/register" />;
 };
