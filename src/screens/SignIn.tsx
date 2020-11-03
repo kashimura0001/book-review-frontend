df50ae65da9c5269b1f1e7cb18de30c931e9e601
keisukekashimura <kashimura@refcome.com>
2020-11-03 21:46:53 +0900

    remove query

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,19 +1,8 @@
 import React, { useState } from "react";
 import firebase from "../Firebase";
 import { Link, useHistory, withRouter } from "react-router-dom";
-import { gql, useQuery } from "@apollo/client";
 
-const GET_CURRENT_USER = gql`
-  query GetCurrentUser {
-    currentUser {
-      id
-      email
-      name
-    }
-  }
-`;
-
-export const SignIn = withRouter((props) => {
+export const SignIn = withRouter(() => {
   const history = useHistory();
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
@@ -26,23 +15,13 @@ export const SignIn = withRouter((props) => {
 
     try {
       await firebase.auth().signInWithEmailAndPassword(email, password);
+      setLoading(false);
+      history.push("/");
     } catch (e) {
       setHasSignInError(true);
       setLoading(false);
       return;
     }
-
-    const { error, data } = await useQuery(GET_CURRENT_USER);
-
-    if (error) {
-      await firebase.auth().signOut();
-      setHasSignInError(true);
-      setLoading(false);
-      return;
-    }
-
-    setLoading(false);
-    history.push(data ? "/" : "/profile/register");
   };
 
   return (
