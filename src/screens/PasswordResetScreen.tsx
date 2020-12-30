b0e65a8f42a992e4f8f377f541c4fde8e2ab8c09
keisukekashimura <kashimura@refcome.com>
2020-12-30 17:07:46 +0900

    add style

--- src/screens/PasswordResetScreen.tsx
+++ src/screens/PasswordResetScreen.tsx
@@ -1,38 +1,51 @@
 import React, { useState } from "react";
+import styles from "./PasswordResetScreen.module.scss";
 import firebase from "../Firebase";
-import { Link, withRouter } from "react-router-dom";
+import { useHistory, withRouter } from "react-router-dom";
 import { SignInPath } from "../routes";
 import { Button } from "../components/atoms/Button";
 import { TextBox } from "../components/atoms/TextBox";
+import { TextButton } from "../components/atoms/TextButton";
+import { NormalText } from "../components/atoms/Text";
 
-export const PasswordResetScreen = withRouter((props) => {
+export const PasswordResetScreen = withRouter(() => {
+  const history = useHistory();
+  const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const onSubmit = () => {
-    firebase
-      .auth()
-      .sendPasswordResetEmail(email)
-      .then(() => {
-        props.history.push(SignInPath);
-      })
-      .catch(() => {
-        alert("送信に失敗しました");
-      });
+  const onSubmit = async () => {
+    setLoading(true);
+    try {
+      await firebase.auth().sendPasswordResetEmail(email);
+      history.push(SignInPath);
+    } catch (e) {
+      setErrorMessage("送信に失敗しました");
+      setLoading(false);
+    }
   };
 
   return (
-    <>
-      <div>
-        <TextBox onChange={(e) => setEmail(e.target.value)} placeholder="パスワードを入力..." />
-      </div>
-      <div>
-        <Button theme="primary" onClick={onSubmit}>
+    <div className={styles.container}>
+      <div className={styles.form}>
+        <NormalText className={styles.title}>パスワード再設定</NormalText>
+        {errorMessage && (
+          <NormalText className={styles.errorMessage} theme="danger">
+            {errorMessage}
+          </NormalText>
+        )}
+        <TextBox
+          className={styles.emailInput}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="メールアドレスを入力..."
+        />
+        <Button className={styles.subjectButton} theme="primary" onClick={onSubmit} disabled={loading || !email}>
           送信
         </Button>
+        <TextButton className={styles.signInPageLink} theme="primary" onClick={() => history.push(SignInPath)}>
+          サインイン画面に戻る
+        </TextButton>
       </div>
-      <div>
-        <Link to={SignInPath}>サインイン画面に戻る</Link>
-      </div>
-    </>
+    </div>
   );
 });
