f1d75b2c6adb330ae6567e1976645d02777f4e56
keisukekashimura <kashimura@refcome.com>
2020-12-30 16:37:53 +0900

    add theme

--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -2,25 +2,40 @@ import React, { FC } from "react";
 import cn from "classnames";
 import styles from "./Text.module.scss";
 
+type TextThemeType = "default" | "primary" | "danger";
+
+const color = (theme: TextThemeType | undefined) => {
+  switch (theme) {
+    case "primary":
+      return styles.primary;
+    case "danger":
+      return styles.danger;
+    case "default":
+    default:
+      return styles.default;
+  }
+};
+
 type Props = {
   id?: string;
+  theme?: TextThemeType;
   className?: string;
 };
 
-export const LighterText: FC<Props> = ({ id, className, children }) => (
-  <span id={id} className={cn(styles.lighterText, className)}>
+export const LighterText: FC<Props> = ({ id, theme, className, children }) => (
+  <span id={id} className={cn(styles.lighterText, color(theme), className)}>
     {children}
   </span>
 );
 
-export const NormalText: FC<Props> = ({ id, className, children }) => (
-  <span id={id} className={cn(styles.normalText, className)}>
+export const NormalText: FC<Props> = ({ id, theme, className, children }) => (
+  <span id={id} className={cn(styles.normalText, color(theme), className)}>
     {children}
   </span>
 );
 
-export const BoldText: FC<Props> = ({ id, className, children }) => (
-  <span id={id} className={cn(styles.boldText, className)}>
+export const BoldText: FC<Props> = ({ id, theme, className, children }) => (
+  <span id={id} className={cn(styles.boldText, color(theme), className)}>
     {children}
   </span>
 );
